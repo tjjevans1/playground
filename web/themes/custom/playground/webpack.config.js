5640007a22871ba420f51c8f2fd28452c144b85d
Tom Evans <tomevans@graphicalliance.co.uk>
2019-07-03 16:46:10 +0100

    Connect react app

--- web/themes/custom/playground/webpack.config.js
+++ web/themes/custom/playground/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
-module.exports = {
+module.exports = [{
+  name: 'main',
   entry: './src/js/index.js',
   output: {
     filename: 'index.js',
@@ -21,4 +22,30 @@ module.exports = {
     ]
   },
   mode: 'development'
-};
+},
+{
+  name: 'app',
+  entry: './src/app/index.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist/js')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      }
+    ]
+  },
+  mode: 'development'
+}];
