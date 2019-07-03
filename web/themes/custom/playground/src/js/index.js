9671b994d85c30b933b433b7093a15fbe9e6fa68
Tom Evans <tomevans@graphicalliance.co.uk>
2019-07-03 14:14:51 +0100

    Add breakpoint utility and grid system

--- web/themes/custom/playground/src/js/index.js
+++ web/themes/custom/playground/src/js/index.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+'use strict';
+
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+import breakpoints from './utilities/breakpoints.js';
+
+(function (Drupal) {
+
+  Drupal.behaviors.playground = {
+    attach: (context, settings) => {
+      // Utils
+      breakpoints();
+    },
+  };
+
+})(Drupal);
---
+++ web/themes/custom/playground/src/js/utilities/breakpoints.js
@@ -0,0 +1,75 @@
+const breakpoints = () => {
+  (function($) {
+
+    const breakpoints = {
+      'xs': [
+        'd-block',
+        'd-sm-none'
+      ],
+      'sm': [
+        'd-none',
+        'd-sm-block',
+        'd-md-none'
+      ],
+      'md': [
+        'd-none',
+        'd-md-block',
+        'd-lg-none'
+      ],
+      'lg': [
+        'd-none',
+        'd-lg-block',
+        'd-xl-none'
+      ],
+      'xl': [
+        'd-none',
+        'd-xl-block'
+      ]
+    };
+
+    const attributes = {
+      breakpoint: 'data-tm-breakpoint'
+    };
+
+    const $body = $('body');
+
+    window.getBreakpoint = () => {
+      return $(`[${attributes.breakpoint}]:visible`).attr(attributes.breakpoint);
+    };
+
+    // Init breakpoint spans
+    let spans = $();
+    Object.keys(breakpoints).forEach(key => {
+      let span = $('<span>')
+        .addClass(breakpoints[key].join(' '))
+        .attr(attributes.breakpoint, key);
+
+      spans = spans.add(span);
+
+    });
+    $body.once('breakpoints').append(spans);
+
+    // Get initial breakpoint on load;
+    let currentBreakpoint = window.getBreakpoint();
+
+    // Add resize event handler in debounce function for increase performance
+    let resize;
+    $(window).once('breakpoints').resize(() => {
+      clearTimeout(resize);
+      resize = setTimeout(() => {
+        let breakpoint = window.getBreakpoint();
+
+        console.log(breakpoint);
+        
+        if (breakpoint !== currentBreakpoint) {
+          const breakpointEvent = new CustomEvent('breakpoint', {detail: breakpoint})
+          window.dispatchEvent(breakpointEvent);
+
+          currentBreakpoint = breakpoint;
+        }
+      }, 50);
+    });
+  })(jQuery);
+};
+
+export default breakpoints;
